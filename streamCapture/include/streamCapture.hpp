295607db6d5c5c899127c4a334a457cccfcab32b
deepinbubblegum <moondeepin@gmail.com>
2022-05-07 19:18:42 +0700

    resize_factor

--- streamCapture/include/streamCapture.hpp
+++ streamCapture/include/streamCapture.hpp
@@ -8,7 +8,7 @@ class streamCapture
 private:
     cv::Mat frame;
     string rtsp_uri = "rtspsrc location=rtsp://admin:Camera123@192.168.100.2:554/rtpstream/config1 latency=0 ! decodebin ! videoconvert ! queue ! appsink sync=true";
-    int resize_factor = 2;
+    int resize_factor = 2.5;
     bool stop = false;
 
 public:
