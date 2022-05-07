badc51a5e79eb40cb4e40c37b795216b54c5a8f2
deepinbubblegum <moondeepin@gmail.com>
2022-05-07 18:07:13 +0700

    update

--- streamCapture/include/streamCapture.hpp
+++ streamCapture/include/streamCapture.hpp
@@ -7,7 +7,7 @@ class streamCapture
 {
 private:
     cv::Mat frame;
-    string rtsp_uri = "rtspsrc location=rtsp://admin:Camera123@192.168.100.2:554/rtpstream/config1 latency=0 ! decodebin ! latency=5 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink";
+    string rtsp_uri = "rtspsrc location=rtsp://admin:Camera123@192.168.100.2:554/rtpstream/config1 latency=100 ! decodebin ! videoconvert ! queue ! appsink sync=true";
     int resize_factor = 3;
 public:
     double width, height;
