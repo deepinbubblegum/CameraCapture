dc8a11ec140629da88d1eafc9e30cf159a6df434
deepinbubblegum <moondeepin@gmail.com>
2022-05-12 16:42:33 +0700

    config

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-gstreamer-1.0-msvc-x86_64-1.20.1
-gstreamer-1.0-devel-msvc-x86_64-1.20.1
-
-rtspsrc location=rtsp://admin:Camera123@192.168.100.2:554/rtpstream/config1 latency=100 ! decodebin ! videoconvert ! queue ! appsink sync=true
+sudo apt-get install --reinstall libstdc++6
--- cross.cmake
+++ cross.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER "arm-none-linux-gnueabihf-gcc.exe")
-set(CMAKE_CXX_COMPILER "arm-none-linux-gnueabihf-g++.exe")
+set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc.exe")
+set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++.exe")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
