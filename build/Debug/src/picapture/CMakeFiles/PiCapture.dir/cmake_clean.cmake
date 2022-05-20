40a64d9ec38fa4291cfa0223a5eae9867be5692b
deepinbubblegum <chaiwit.p@groupmaker.co.th>
2022-05-20 15:38:07 +0700

    remove

---
+++ build/Debug/src/picapture/CMakeFiles/PiCapture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/PiCapture"
+  "../../bin/PiCapture.pdb"
+  "CMakeFiles/PiCapture.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PiCapture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
