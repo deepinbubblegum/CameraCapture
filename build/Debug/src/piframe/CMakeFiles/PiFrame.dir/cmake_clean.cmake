89ec1ab7d9e4cd0c5fb3aba13ad6b7604cbe4f48
deepinbubblegum <chaiwit.p@groupmaker.co.th>
2022-05-20 15:39:41 +0700

    remove

---
+++ build/Debug/src/piframe/CMakeFiles/PiFrame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/PiFrame"
+  "../../bin/PiFrame.pdb"
+  "CMakeFiles/PiFrame.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PiFrame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Debug/src/piframe/CMakeFiles/PiFrame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PiFrame.
+# This may be replaced when dependencies are built.
