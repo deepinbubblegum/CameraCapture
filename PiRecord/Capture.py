9e79a1c8bdfb3bd65bce9200c1b882cb7b25a2da
deepinbubblegum <chaiwit.p@groupmaker.co.th>
2022-05-25 21:52:12 +0700

    update

--- PiRecord/Capture.py
+++ PiRecord/Capture.py
@@ -5,9 +5,8 @@ from threading import Thread
 from queue import Queue
 import atexit
 
-class Capture:
+class Capture():
     def __init__(self, width=1920, height=1080, fps=50):
-        print("__init Camera")
         self.queue_frame = Queue()
         self.isRuning = False
         self.width = width
@@ -16,28 +15,32 @@ class Capture:
         self.width2 = int(64*(self.width/64))
         self.height2 = int(32*(self.height/32))
         self.bytesPerFrame = int(self.width2*self.height2*3/2)
+        print(f'width={self.width}, height={self.height}, fps="{self.fps}, bytesPerFrame={self.bytesPerFrame}')
 
     def update(self):
         videoCmd = f'libcamera-vid -n --framerate {self.fps} --width {self.width} --height {self.height} -t 0 --codec yuv420 -o -'
         videoCmd = videoCmd.split()
-        cameraProcess = sp.Popen(videoCmd, stdout=sp.PIPE, bufsize=1)
+        cameraProcess = sp.Popen(videoCmd, stdout=sp.PIPE, bufsize=30)
         atexit.register(cameraProcess.terminate)
-        rawStream = cameraProcess.stdout.read(self.bytesPerFrame)
         while self.isRuning:
-            e1 = cv2.getTickCount()
-            cameraProcess.stdout.flush()
             yuv = np.frombuffer(cameraProcess.stdout.read(self.bytesPerFrame), dtype=np.uint8).reshape((self.height2*3//2, self.width2))
             if yuv.size != self.bytesPerFrame:
                 print("Error: Camera stream closed unexpectedly")
                 break
             self.queue_frame.put(yuv)
-            e2 = cv2.getTickCount()
-            time = (e2 - e1)/ cv2.getTickFrequency()
-            print("\033[2J\033[1;1H")
-            print(1/time)
+            cameraProcess.stdout.flush()
+
+    def read(self):
+        return self.queue_frame.get()
+
+    def ret(self):
+        return self.queue_frame.qsize() > 0
 
     def start(self):
         self.isRuning = True
+        thread_cap = Thread(target=self.update, args=())
+        thread_cap.daemon = True
+        thread_cap.start()
 
     def stop(self):
         self.isRuning = False
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-from PiRecord import Captrue
+from PiRecord import Capture
+import cv2
+import time
+
+cap_ = Capture(width=1920, height=1080, fps=50)
+cap_.start()
+N_frames = 0
+MAX_frames = 50 * 10
+start_time = time.time()
+# cv2.cvtColor(cap_.read(), cv2.COLOR_YUV2BGR_I420)
+while True:
+    if cap_.ret:
+        cap_.read()
+        N_frames += 1
+    end_time = time.time()
+    elapsed = end_time-start_time
+    print("\033[2J\033[1;1H Result: "+str(N_frames/elapsed)+" fps")
+    if N_frames > MAX_frames:
+        N_frames = 0
+        start_time = time.time()
