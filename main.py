8028de19d8ad83f7f131165ae3b04ffae0d64481
deepinbubblegum <chaiwit.p@groupmaker.co.th>
2022-05-28 17:29:02 +0700

    update

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from threading import Thread
 width=1920
 height=1080
 fps=50
+
+# Multi Thread
 # cap_ = Capture(width, height, fps)
 # cap_.start()
 # while True:
@@ -14,5 +16,6 @@ fps=50
 #     else:
 #         sleep(1)
 
+Multi proc shm
 cap_ = CaptureMultiproc(width, height, fps)
 cap_.start()
