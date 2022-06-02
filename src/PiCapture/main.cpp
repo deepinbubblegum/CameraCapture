4828095e52bf0fc4a25d4be7e2ace4787a2d7f86
deepinbubblegum <chaiwit.p@groupmaker.co.th>
2022-06-02 20:06:48 +0700

    update

--- src/PiCapture/main.cpp
+++ src/PiCapture/main.cpp
@@ -5,23 +5,6 @@
 
 using namespace std;
 int main(){
-    streamCapture stream;
-    cout << "################# CameraCapture Pi version #################" << endl;
-    int capture_width = 1920; //1280 ;
-    int capture_height = 1080; //720 ;
-    int framerate = 50;
-    // int display_width = 1920; //640 ;
-    // int display_height = 1080; //360 ;
-
-    stream.setParamsCapture(capture_width, capture_height, framerate);
-    stream.setStartCapture();
-
-    while(true){
-        vector<string> file_list = stream.getPathFileVideo();
-        // cout << "#################" << endl;
-        for(string str : file_list)
-            cout << str << endl;
-        this_thread::sleep_for(10000ms);
-    }
+    cout << "hello" << endl;
     return 0;
 }
